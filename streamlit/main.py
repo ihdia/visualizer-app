0563066fa014e706fe6c58c8c68df96975f0dcc0
Bala <balavarun.pedapudi@research.iiit.ac.in>
2020-12-08 11:38:36 +0530

    Replacing %20 with space

--- streamlit/main.py
+++ streamlit/main.py
@@ -7,6 +7,7 @@ import cv2
 import matplotlib.pyplot as pl
 import json
 
+# Loading jsons
 with open('/home/bala/Desktop/iiith/visualizer-app/raw/test/combined_test_converted.json', 'r') as f:
     test_data = json.load(f)
 
@@ -16,10 +17,14 @@ with open('/home/bala/Desktop/iiith/visualizer-app/raw/train/combined_train_conv
 with open('/home/bala/Desktop/iiith/visualizer-app/raw/train_val/combined_val_converted.json', 'r') as f:
     train_val_data = json.load(f)
 
-st.write(train_val_data[20]['image_url'][22:])
-#st.write(train_val_data[20])
+# Picking a random image from train val, extracting location
+image_directory = train_val_data[20]['image_url'][22:]
+image_directory = image_directory.replace("%20"," ")
 
-image_path = '/home/bala/Desktop/iiith/visualizer-app/new_jpg_data/Bhoomi_data/images/AAVARNI VYAKHYA/GOML/991/19.jpg'
+st.write(image_directory)
+
+# Finding image path
+image_path = '/home/bala/Desktop/iiith/visualizer-app/new_jpg_data/' + image_directory
 
 image = pl.imread(image_path)
 st.image(image)
