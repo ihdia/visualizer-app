4b3852e2aef8335fb0fc612568c4cdb45bfe4d38
Bala <balavarun.pedapudi@research.iiit.ac.in>
2020-12-10 12:32:15 +0530

    Displaying test train val image

--- streamlit/main.py
+++ streamlit/main.py
@@ -15,42 +15,52 @@ with open('../raw/train/combined_train_converted.json', 'r') as f:
 with open('../raw/train_val/combined_val_converted.json', 'r') as f:
     train_val_data = json.load(f)
 
-info = train_data[20]
 
-# Picking a random image from train val, extracting location
-image_directory = train_val_data[20]['image_url'][22:]
-image_directory = image_directory.replace("%20"," ")
 
-#st.write(image_directory)
 
-# Finding image path. To be replaced with server directiory
-image_path = '/home/user/hdia/abhitrivedi/BoundaryNet/new_jpg_data' + image_directory
-print(image_path)
+def update_image_info(value):
+
+    if(value == 'Train'):
+        data = train_data
+    elif(value == 'Test'):
+        data = test_data
+    elif(value == 'Validation'):
+        data = train_val_data
+    else:
+        return 'null', 'null'
+
+
+    info = data[20]
+    
+    # Picking a random image from data, extracting location
+    image_directory = data[20]['image_url'][22:]
+    image_directory = image_directory.replace("%20"," ")
+    
+    
+    # Finding image path. To be replaced with server directiory
+    image_path = '../new_jpg_data' + image_directory
+
+    return info, image_path
+
 
 # Sidebar elements
 st.sidebar.title('Select Image')
-image_selector = st.sidebar.radio('Image Type', ['Train', 'Test', 'Validation'])
+image_selector = st.sidebar.radio('Image Type', ['None', 'Train', 'Test', 'Validation'])
 
-# analyze_button = st.sidebar.button('Analyze')
-
-# if(analyze_button):
-#     result.app(image_selector)
-# else:
-#     #st.write('App')
+info, image_path = update_image_info(image_selector)
 
 
-
-
-
-st.title('Image list')
-    # st.image(image)
-
-c1,c2 = st.beta_columns(2)
-
-gt_pts = c1.checkbox("GT-points")
-gt_mask = c1.checkbox("GT-mask")
-
-fig,label = image_list.app(image_path,info,gt_pts,gt_mask)
-
-c2.write(label)
-c2.plotly_chart(fig)
+if(info != 'null'):
+    st.title('Image list')
+    print('INFO IS ', info)
+    c1,c2 = st.beta_columns(2)
+    
+    gt_pts = c1.checkbox("GT-points")
+    gt_mask = c1.checkbox("GT-mask")
+    
+    fig,label = image_list.app(image_path,info,gt_pts,gt_mask)
+    
+    c2.write(label)
+    c2.plotly_chart(fig)
+if(info == 'null'):
+    st.title('Select an option')
