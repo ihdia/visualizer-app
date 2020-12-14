f0235998381a9a33bff848eb6de31c6f274d3bb3
Bala <balavarun.pedapudi@research.iiit.ac.in>
2020-12-14 14:04:29 +0530

    Session state in app, prev and next buttons should work only for validation

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,51 +1,76 @@
 import image_list
 import result
+import SessionState
 
 import streamlit as st
 import numpy as np
 import json
 
 # Loading jsons, directories need to be replaced aptly
-with open('../raw/test/combined_test_converted.json', 'r') as f:
+with open('/home/bala/Desktop/iiith/visualizer-app/raw/test/combined_test_converted.json', 'r') as f:
     test_data = json.load(f)
 
-with open('../raw/train/combined_train_converted.json', 'r') as f:
+with open('/home/bala/Desktop/iiith/visualizer-app/raw/train/combined_train_converted.json', 'r') as f:
     train_data = json.load(f)
 
-with open('../raw/train_val/combined_val_converted.json', 'r') as f:
+with open('/home/bala/Desktop/iiith/visualizer-app/raw/train_val/combined_val_converted.json', 'r') as f:
     train_val_data = json.load(f)
 
+session_state = SessionState.get(name='', counter=None)
 
-def update_image_info(value):
 
+def filter_json(value, session_state):
     if(value == 'Train'):
         data = train_data
     elif(value == 'Test'):
         data = test_data
+        session_state.counter = 20
     elif(value == 'Validation'):
         data = train_val_data
     else:
         return 'null', 'null'
 
 
-    info = data[20]
-    
-    # Picking a random image from data, extracting location
-    image_directory = data[20]['image_url'][22:]
-    image_directory = image_directory.replace("%20"," ")
-    
-    
-    # Finding image path. To be replaced with server directiory
-    image_path = '../new_jpg_data' + image_directory
+
+    return data
+
+def update_image_info(data):
+
+    counter = session_state.counter
+
+
+    if(counter > 0):
+        info = data[counter]
+        
+        # Picking a random image from data, extracting location
+        image_directory = data[counter]['image_url'][22:]
+        image_directory = image_directory.replace("%20"," ")
+        
+        
+        # Finding image path. To be replaced with server directiory
+        image_path = '/home/bala/Desktop/iiith/visualizer-app/new_jpg_data' + image_directory
+        print(image_path)
+    else:
+        return 'null', 'null'
+
 
     return info, image_path
+
 
 
 # Sidebar elements
 st.sidebar.title('Select Image')
 image_selector = st.sidebar.radio('Image Type', ['None', 'Train', 'Test', 'Validation'])
 
-info, image_path = update_image_info(image_selector)
+
+if st.sidebar.button('Prev'):
+    session_state.counter -= 1
+if st.sidebar.button('Next'):
+    session_state.counter += 1
+
+json_selected = filter_json(image_selector, session_state)
+
+info, image_path = update_image_info(json_selected)
 
 
 if(info != 'null'):
@@ -54,16 +79,12 @@ if(info != 'null'):
     
     gt_pts = c1.checkbox("GT-points")
     gt_mask = c1.checkbox("GT-mask")
-    
+
     fig,label = image_list.app(image_path,info,gt_pts,gt_mask)
     
     c2.write(label)
     c2.plotly_chart(fig)
 
-    with st.beta_container():
-        col1, col2 = st.beta_columns(2)
-        col1.button('Prev')
-        col2.button('Next')
 
 
 if(info == 'null'):
