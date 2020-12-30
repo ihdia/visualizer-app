975332a653b157f20ca3d059a9684ae1f79dd5e5
Ravi Kiran <user@cvit.iiit.ac.in>
2020-12-30 21:12:44 +0530

    Fixing zooming of GT points

--- streamlit/image_list.py
+++ streamlit/image_list.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import cv2 as cv
+from PIL import Image
+
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 
 def app(image_path,info):
     image = cv.imread(image_path)
+
     
     label = info['label']
     # comp = info['components'][0]
@@ -38,16 +41,89 @@ def app(image_path,info):
     pts[:,1] -= y0
 
     image = cv.cvtColor(image,cv.COLOR_RGB2BGR)
+    pil_image = Image.fromarray(image)
 
-    fig = go.Figure(go.Image(z=image))
-    fig.update_xaxes(showticklabels=False)
-    fig.update_yaxes(showticklabels=False)
+    layout = go.Layout(
+        autosize=False,
+        width=1000,
+        height=1000,
+    
+        xaxis= go.layout.XAxis(linecolor = 'black',
+                              linewidth = 1,
+                              mirror = True),
+    
+        yaxis= go.layout.YAxis(linecolor = 'black',
+                              linewidth = 1,
+                              mirror = True),
+    
+        margin=go.layout.Margin(
+            l=50,
+            r=50,
+            b=100,
+            t=100,
+            pad = 4
+        )
+    )
 
-    fig.add_trace(go.Scatter(x=np.take(pts,0,axis=1),y=np.take(pts,1,axis=1),line_color='blue',name="GT-pts",visible='legendonly'))    
-    fig.add_trace(go.Scatter(x=np.take(pts,0,axis=1),y=np.take(pts,1,axis=1),fill="toself",mode='none',name="GT-mask",fillcolor='rgba(128,0,128,0.3)',visible='legendonly'))
+
+    # Constants
+    img_width = 1600
+    img_height = 900
+    scale_factor = 0.5
+
+    fig = go.Figure()
+
+
+    actual_height, actual_width, _ = image.shape
+    x_zoom = (np.take(pts,0,axis=1) * img_width * scale_factor / actual_width)
+    y_zoom = img_height * scale_factor - (np.take(pts,1,axis=1) * img_height * scale_factor / actual_height)
+
+    print(x_zoom, y_zoom)
+
+
+    fig.add_trace(go.Scatter(x=np.take(pts,0,axis=1) * img_width * scale_factor / actual_width,y=np.take(pts,1,axis=1) * img_height * scale_factor / actual_height,line_color='blue',name="GT-pts",visible='legendonly'))    
+    fig.add_trace(go.Scatter(x = x_zoom,y = y_zoom,fill="toself",mode='none',name="GT-mask",fillcolor='rgba(128,0,128,0.3)',visible='legendonly'))
     fig.add_trace(go.Scatter(x=np.take(encoder_pts,0,axis=1),y=np.take(encoder_pts,1,axis=1),line_color='green',name='encoder output',visible='legendonly'))
     fig.add_trace(go.Scatter(x=np.take(encoder_pts,0,axis=1),y=np.take(encoder_pts,1,axis=1),fill="toself",mode='none',name="MCNN-mask",fillcolor='rgba(255,255,0,0.3)',visible='legendonly'))
     fig.add_trace(go.Scatter(x=np.take(gcn_pts,0,axis=1),y=np.take(gcn_pts,1,axis=1),line_color='red',name='GCN output',visible='legendonly'))
+
+
+    # Configure axes
+    fig.update_xaxes(
+        visible=False,
+        range=[0, img_width * scale_factor]
+    )
     
+    fig.update_yaxes(
+        visible=False,
+        range=[0, img_height * scale_factor],
+        # the scaleanchor attribute ensures that the aspect ratio stays constant
+        scaleanchor="x"
+    )
+
+    # Add image
+    fig.add_layout_image(
+        dict(
+            x=0,
+            sizex=img_width * scale_factor,
+            y=img_height * scale_factor,
+            sizey=img_height * scale_factor,
+            xref="x",
+            yref="y",
+            opacity=1.0,
+            layer="below",
+            sizing="stretch",
+            source=pil_image)
+    )
+    # Configure other layout
+    fig.update_layout(
+        width=img_width * scale_factor,
+        height=img_height * scale_factor,
+        margin={"l": 0, "r": 0, "t": 0, "b": 0},
+    )
+
+
+
+ 
     
     return fig,label,iou,hd
